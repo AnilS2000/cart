c85fbaeffcf03c0030646ee823a5565922852fcd
AnilS18 <anilkumar.saragadam@cogoport.com>
2022-06-07 00:24:35 +0530

    second

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import "./styles.css";
 
 <link rel="stylesheet" href="mystyle.css" />
 function App() {
-    var rs = 1000000000
+    var rs = 4000000000
     const [value, setValue] = useState(0);
     const subvalue = () => {
         if (value - 1 < 0)
@@ -19,16 +19,16 @@ function App() {
     return (
         <>
             <div className="text">
-                <h1>{rs - value* 100}</h1>
+                <h1>{rs - value* 70}</h1>
             </div>
-            <div className="centered">
+            <div className="image">
                 <div>
                     <div>
                         <img
                             src="https://m.media-amazon.com/images/I/61pj9OP0IQL._SX522_.jpg" />
                     </div>
                     <div class="text">
-                        <h3>100$</h3>
+                        <h3>60rs</h3>
                         <button onClick={() => subvalue()}>-</button>
                         <input type="number" value={value} />
                         <button onClick={() => addvalue()}>+</button>
